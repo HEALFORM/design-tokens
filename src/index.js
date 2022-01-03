3b8b7df4168f49da47442d2005d3c5b6a57f4978
Domenik Töfflinger <domenikt96@yahoo.de>
2022-01-03 16:56:30 +0100

    fix: 'require' typos in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 module.exports = {
   animations: require('./animations'),
   borders: require('./borders'),
-  breakpoints: require('./breakpoint'),
-  colors: require('./color'),
+  breakpoints: require('./breakpoints'),
+  colors: require('./colors'),
   fonts: require('./fonts'),
   radius: require('./radius'),
   shadows: require('./shadows'),
