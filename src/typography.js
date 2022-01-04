0ee675f27ab1536b261b43401140adca0dde6b5a
Domenik Töfflinger <domenikt96@yahoo.de>
2022-01-04 14:51:40 +0100

    fix: Refactor typography.js that every object has a value

--- src/typography.js
+++ src/typography.js
@@ -2,57 +2,105 @@
 module.exports = {
   headline: {
     one: {
-      fontSize: "32px",
-      lineHeight: "36px"
+      fontSize: {
+        value: "32px"
+      },
+      lineHeight: {
+        value: "36px"
+      }
     },
     two: {
-      fontSize: "24px",
-      lineHeight: "28px"
+      fontSize: {
+        value: "24px"
+      },
+      lineHeight: {
+        value: "28px"
+      }
     },
     three: {
-      fontSize: "20px",
-      lineHeight: "24px"
+      fontSize: {
+        value: "20px"
+      },
+      lineHeight: {
+        value: "24px"
+      }
     },
     four: {
-      fontSize: "18px",
-      lineHeight: "24px"
+      fontSize: {
+        value: "18px"
+      },
+      lineHeight: {
+        value: "24px"
+      }
     },
   },
   title: {
     one: {
-      fontSize: "120px",
-      lineHeight: "120px"
+      fontSize: {
+        value: "120px"
+      },
+      lineHeight: {
+        value: "120px"
+      }
     },
     two: {
-      fontSize: "96px",
-      lineHeight: "96px"
+      fontSize: {
+        value: "96px"
+      },
+      lineHeight: {
+        value: "96px"
+      }
     },
     three: {
-      fontSize: "64px",
-      lineHeight: "64px"
+      fontSize: {
+        value: "64px"
+      },
+      lineHeight: {
+        value: "64px"
+      }
     },
     four: {
-      fontSize: "56px",
-      lineHeight: "56px"
+      fontSize: {
+        value: "56px"
+      },
+      lineHeight: {
+        value: "56px"
+      }
     },
   },
   subHeadline: {
-    fontSize: "14px",
-    lineHeight: "20px"
+    fontSize: {
+      value: "14px"
+    },
+    lineHeight: {
+      value: "20px"
+    }
   },
   body: {
     one: {
-      fontSize: "16px",
-      lineHeight: "24px"
+      fontSize: {
+        value: "16px"
+      },
+      lineHeight: {
+        value: "24px"
+      }
     },
     two: {
-      fontSize: "14px",
-      lineHeight: "20px"
+      fontSize: {
+        value: "14px"
+      },
+      lineHeight: {
+        value: "20px"
+      }
     },
   },
   bodyLarge: {
-    fontSize: "20px",
-    lineHeight: "28px"
+    fontSize: {
+      value: "20px"
+    },
+    lineHeight: {
+      value: "28px"
+    }
   }
 }
 /* eslint-disable sort-keys */
