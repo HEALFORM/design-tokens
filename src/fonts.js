2a2bb92bbe20167a11eb243a42ee66cfb4abbc28
Domenik Töfflinger <domenikt96@yahoo.de>
2022-04-13 14:12:55 +0200

    fix: Rename `fontStack` design-tokens to `heading` and `body`

--- src/fonts.js
+++ src/fonts.js
@@ -1,10 +1,10 @@
 /* eslint-disable sort-keys */
 module.exports = {
   fontStack: {
-    special: {
+    heading: {
       value: "Space Grotesk"
     },
-    default: {
+    body: {
       value: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
     },
     mono: {
