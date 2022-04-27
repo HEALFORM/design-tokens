8d938e8ce2a720351954be152dbfa4b4206d72b5
Domenik Töfflinger <domenikt96@yahoo.de>
2022-04-27 12:54:47 +0200

    fix: Add missing border token values.

--- src/borders.js
+++ src/borders.js
@@ -1,3 +1,15 @@
 /* eslint-disable sort-keys */
 
+module.exports = {
+  none: {
+    value: "0px"
+  },
+  sm: {
+    value: "1px"
+  },
+  md: {
+    value: "2px"
+  }
+}
+
 /* eslint-disable sort-keys */
